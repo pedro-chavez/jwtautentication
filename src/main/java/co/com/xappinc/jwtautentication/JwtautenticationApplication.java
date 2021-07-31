904136795b7a37fdb92b765fc0fea216497a142e
Pedro Luis <plchavez@ucundinamarca.edu.co>
2021-07-30 20:00:59 -0500

    first commit

---
+++ src/main/java/co/com/xappinc/jwtautentication/JwtautenticationApplication.java
@@ -0,0 +1,13 @@
+package co.com.xappinc.jwtautentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtautenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtautenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/xappinc/jwtautentication/JwtautenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.xappinc.jwtautentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtautenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
