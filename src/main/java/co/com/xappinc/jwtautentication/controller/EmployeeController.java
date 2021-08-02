5113bc5aa2dd90cff61e1fab172b1ba720ac8983
Pedro Luis <plchavez@ucundinamarca.edu.co>
2021-08-02 07:25:21 -0500

    JWT base

---
+++ src/main/java/co/com/xappinc/jwtautentication/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package co.com.xappinc.jwtautentication.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@RequestMapping({ "/greeting" })
+	public String welcomePage() {
+		return "Welcome!";
+	}
+
+}
